546607178208439c1b77905718ac6b7a61373799
Trecto34 <trecto34@protonmail.com>
2025-07-09 11:53:21 -0300

    Nivel Mestre

--- xadrez.c
+++ xadrez.c
@@ -1,48 +1,60 @@
 #include <stdio.h>
 
-void torre() {
-    printf("-=Torre=-\n");
-    for (int i = 0; i<5; i++){
-        printf("Direita\n");
+void torre(int num_move){
+    if (num_move <= 0){
+        return;
     }
+    printf("Direita\n");
+    torre(num_move - 1);
 }
 
-void bispo() {
-    printf("-=Bispo=-\n");
-    for (int i = 0; i<5; i++){
-        printf("Cima, direita\n");
+void bispo(int num_move) {
+    if (num_move <= 0){
+        return;
     }
+
+    for (int i = 0; i<1; i++){
+        printf("Cima, ");
+        for (int j = 0; j<1; j++){
+            printf("Direita\n");
+        }
+    }
+    bispo(num_move - 1);
 }
 
-
-void rainha() {
-    printf("-=Rainha=-\n");
-    for (int i = 0; i<8; i++){
-        printf("Esquerda\n");
+void rainha(int num_move) {
+    if (num_move <= 0){
+        return;
     }
+    printf("Esquerda\n");
+    rainha(num_move -1);
 }
 
 void cavalo(){
     int baixo = 2;
     int esquerda = 1;
-    int i = 0;
 
-    printf("-=CAVALO=-\n");
-    while (i<baixo) {
-        printf("Baixo, ");
-        i++;
-        if (i == 2) {
-            for (int j = 0; j<esquerda; j++){
-                printf("Esquerda\n");
-            }
+    for (int i=0, j=0; i<baixo || j<esquerda;) {
+        if (i < 2){
+            printf("Cima\n");
+            i++;
+        } else if (j < 1) {
+            printf("Direita\n");
+            j++;
         }
     }
 }
 
 int main() {
-    torre();
-    bispo();
-    rainha();
+    printf("-=TORRE=-\n");
+    torre(5);
+
+    printf("-=BISPO=-\n");
+    bispo(5);
+    printf("-=RAINHA=-\n");
+    rainha(8);
+
+    printf("-=CAVALO=-\n");
     cavalo();
     return 0;
 }
